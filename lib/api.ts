45c333131afd1ab2239f140d460c01f4c3fbfcbc
Luke Scanlan <lukescanlanatwork@gmail.com>
2022-05-05 13:17:09 +0100

    Replace fetch with axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,33 @@
-const getStrapiURL = (path = "") => {
-  return `${process.env.API_URL || "http://localhost:1337/api"}${path}`;
-};
+import axios from "axios";
+
+const getStrapiURL = () =>
+  `${process.env.API_URL || "http://localhost:1337/api"}`;
+
+const api = axios.create({
+  baseURL: getStrapiURL(),
+  timeout: 1000,
+});
 
 // Helper to make GET requests to Strapi
-const getFromAPI = async (path: string, queryString = "populate=*") => {
-  const requestUrl = getStrapiURL(`${path}?${queryString}`);
-  const response = await fetch(requestUrl);
-  const data = await response.json();
-  return data;
+const getFromAPI = async <T>(path: string, queryString = "populate=*") => {
+  try {
+    const requestUrl = `${path}?${queryString}`;
+
+    const response = await api.get<T>(requestUrl);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (process.env.NODE_ENV !== "production") {
+        console.log("http request failed");
+        console.log(error);
+      }
+    } else {
+      if (process.env.NODE_ENV !== "production") {
+        console.log("unexpected error");
+        console.log(error);
+      }
+    }
+  }
 };
 
 export { getStrapiURL, getFromAPI };
