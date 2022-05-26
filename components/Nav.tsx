62110f57a189c65edb4ea22dfcd531d74bad1678
Luke Scanlan <lukescanlanatwork@gmail.com>
2022-05-26 05:35:31 +0100

    Fix nav flash by adding 100ms timeout to transitions

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
-import { Theme, css, useTheme } from "@emotion/react";
+import { Theme, css } from "@emotion/react";
 
-import Collapse from "@mui/material/Collapse";
-import { Sling as Hamburger } from "hamburger-react";
-import NavButton from "./NavButton";
+import NavMenu from "./NavMenu";
 import NavPage from "../types/NavPage";
 import Sitename from "./Sitename";
-import { useMediaQuery } from "@mui/material";
-import { useState } from "react";
 import useWindowDimensions from "../lib/useWindowDimensions";
 
 interface NavProps {
@@ -45,69 +41,11 @@ const navStyles = (theme: Theme) =>
 
 const Nav = ({ navPages, spacing = 1 }: NavProps) => {
   const windowDimensions = useWindowDimensions();
-  const theme = useTheme();
-  const [isOpen, setIsOpen] = useState(false);
-  const showMenuButton = useMediaQuery(theme.breakpoints.down("sm"));
-
-  const spacingStyles = () =>
-    css({
-      ".buttons .MuiCollapse-wrapperInner": {
-        gap: theme.spacing(spacing),
-        padding: showMenuButton
-          ? theme.spacing(0, 0, spacing)
-          : theme.spacing(spacing, 0, 0),
-      },
-    });
 
   return (
-    <nav css={[navStyles, spacingStyles]} className="twelve-column">
+    <nav css={navStyles} className="twelve-column">
       <Sitename textWidth={windowDimensions.width} />
-      <div className="nav-menu">
-        <Collapse
-          in={showMenuButton}
-          css={{
-            ".MuiCollapse-wrapperInner": {
-              display: "grid",
-              justifyContent: "center",
-            },
-          }}
-          unmountOnExit
-          timeout={theme.transitions.duration.shorter}
-        >
-          <Hamburger
-            toggled={isOpen}
-            toggle={setIsOpen}
-            easing={theme.transitions.easing.easeInOut}
-            label="Show menu"
-            hideOutline={false}
-            size={30}
-          />
-        </Collapse>
-        <Collapse
-          in={isOpen || !showMenuButton}
-          timeout={theme.transitions.duration.shorter}
-          unmountOnExit
-          className="buttons"
-        >
-          {navPages
-            ? navPages.map((navPage, i) => {
-                const href =
-                  navPage.attributes.slug === "about"
-                    ? "/about"
-                    : `/category/${navPage.attributes.slug}`;
-                return (
-                  <NavButton
-                    text={navPage.attributes.name}
-                    href={href}
-                    key={i.toString()}
-                  />
-                );
-              })
-            : Array.from<number>({ length: 4 }).map((_, i) => (
-                <NavButton key={i.toString()} />
-              ))}
-        </Collapse>
-      </div>
+      <NavMenu navPages={navPages} spacing={spacing} />
     </nav>
   );
 };
